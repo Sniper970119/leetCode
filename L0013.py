ccd3989fc3670edb4ced6742d5a9a3c951045ade
Sniper970119 <359366783@qq.com>
2020-01-17 09:38:16 +0800

    L0013

---
+++ L0013.py
@@ -0,0 +1,49 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+
+
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        alpha_num_dict = {
+            'M': 1000,
+            'D': 500,
+            'C': 100,
+            'L': 50,
+            'X': 10,
+            'V': 5,
+            'I': 1
+        }
+        sum = 0
+        for i in range(len(s)):
+            if i < len(s) - 1 and alpha_num_dict[s[i]] < alpha_num_dict[s[i + 1]]:
+                sum -= alpha_num_dict[s[i]]
+            else:
+                sum += alpha_num_dict[s[i]]
+        return sum
+
+
+if __name__ == '__main__':
+    s = 'IV'
+    print(Solution().romanToInt(s))
