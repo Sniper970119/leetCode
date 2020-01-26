83a48a5277a906bb60c5849970656c6988af594d
Sniper970119 <359366783@qq.com>
2020-01-26 23:35:59 +0800

    L0025

---
+++ L0001~L0050/L0025.py
@@ -0,0 +1,64 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def reverseKGroup(self, head: ListNode, k: int):
+        """
+        :type head: ListNode
+        :type k: int
+        :rtype: ListNode
+        """
+        cur = head
+        count = 0
+        while cur and count != k:
+            cur = cur.next
+            count += 1
+        if count == k:
+            cur = self.reverseKGroup(cur, k)
+            while count:
+                tmp = head.next
+                head.next = cur
+                cur = head
+                head = tmp
+                count -= 1
+            head = cur
+        return head
+
+
+if __name__ == '__main__':
+    n1 = ListNode(1)
+    n2 = ListNode(2)
+    n3 = ListNode(3)
+    n4 = ListNode(4)
+    n5 = ListNode(5)
+    n1.next = n2
+    n2.next = n3
+    n3.next = n4
+    n4.next = n5
+    print(Solution().reverseKGroup(n1, 5))
