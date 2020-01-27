c90478898258cdee0854c108df300746a821d8be
Sniper970119 <359366783@qq.com>
2020-01-27 12:32:48 +0800

    L0028

---
+++ L0001~L0050/L0028.py
@@ -0,0 +1,37 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+
+
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: stra
+        :rtype: int
+        """
+        return haystack.find(needle)
+
+
+if __name__ == '__main__':
+    haystack = "hello"
+    needle = "ll"
+    help(str.find)
+    print(Solution().strStr(haystack, needle))
