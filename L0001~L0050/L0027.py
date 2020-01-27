43fb977c03abf45199f6ad18ba40abefe0c0abdd
Sniper970119 <359366783@qq.com>
2020-01-27 12:12:23 +0800

    L0027

---
+++ L0001~L0050/L0027.py
@@ -0,0 +1,38 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+
+
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        for i in range(len(nums) - 1, -1, -1):
+            if nums[i] == val:
+                nums.pop(i)
+        return len(nums)
+
+
+if __name__ == '__main__':
+    nums = [3, 2, 2, 3]
+    print(Solution().removeElement(nums, 3))
