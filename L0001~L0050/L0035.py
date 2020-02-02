a0da092f46ca9af30a4358650ee99e6e0eff17c0
Sniper970119 <359366783@qq.com>
2020-02-02 13:55:10 +0800

    L0035

---
+++ L0001~L0050/L0035.py
@@ -0,0 +1,50 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+
+
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        if len(nums) == 0:
+            return 0
+        if nums[0] > target:
+            return 0
+        if nums[-1] < target:
+            return len(nums)
+        if nums[0] == target:
+            return 0
+        for i in range(len(nums) - 1):
+            if nums[i] < target < nums[i + 1]:
+                return i + 1
+            if nums[i] == target:
+                return i
+            if nums[i + 1] == target:
+                return i + 1
+
+
+if __name__ == '__main__':
+    nums = [1]
+    target = 1
+    print(Solution().searchInsert(nums, target))
