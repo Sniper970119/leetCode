bcd48ebe7605c59e4cdf84e54c3a4d8d9447f270
Sniper970119 <359366783@qq.com>
2020-02-04 14:59:29 +0800

    L0039

---
+++ L0001~L0050/L0039.py
@@ -0,0 +1,59 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+
+
+class Solution(object):
+    def combinationSum(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        candidates.sort()
+        n = len(candidates)
+        res = []
+
+        def backtrack(i, tmp_sum, tmp):
+            """
+            回溯法
+            :param i: 当前选择的索引
+            :param tmp_sum: 当前已选择所有数的和
+            :param tmp: 当前已选择数的列表
+            :return:
+            """
+            if tmp_sum > target or i == n:
+                return
+            if tmp_sum == target:
+                res.append(tmp)
+                return
+            for j in range(i, n):
+                if tmp_sum + candidates[j] > target:
+                    break
+                backtrack(j, tmp_sum + candidates[j], tmp + [candidates[j]])
+
+        backtrack(0, 0, [])
+        return res
+
+
+if __name__ == '__main__':
+    candidates = [2, 3, 6, 7]
+    target = 7
+    print(Solution().combinationSum(candidates, target))
