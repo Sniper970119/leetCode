487c4aaca32821640a6dfb8fde9d81379945b786
Sniper970119 <359366783@qq.com>
2020-02-05 14:07:53 +0800

    L0041

---
+++ L0001~L0050/L0041.py
@@ -0,0 +1,42 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+
+
+class Solution(object):
+    def firstMissingPositive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        temp_list = [0 for _ in range(n)]
+        for each in nums:
+            if each <= n and each > 0:
+                temp_list[each - 1] = each
+        for i in range(n):
+            if temp_list[i] != i + 1:
+                return i + 1
+        return n + 1
+
+
+if __name__ == '__main__':
+    nums = [1]
+    print(Solution().firstMissingPositive(nums))
