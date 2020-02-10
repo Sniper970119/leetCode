2d976b73efe421a55711898f88937d8a45181242
Sniper970119 <359366783@qq.com>
2020-02-10 17:37:54 +0800

    L0046

---
+++ L0001~L0050/L0046.py
@@ -0,0 +1,47 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+
+
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+
+        def backtrack(first=0):
+            if first == n:
+                output.append(nums[:])
+            for i in range(first, n):
+                # 这里使用交换来模拟递归其余数字（比如1,2,3。 交换1 2   13  就模拟了第一个位置的全部可能）
+                nums[first], nums[i] = nums[i], nums[first]
+                backtrack(first + 1)
+                nums[first], nums[i] = nums[i], nums[first]
+
+        n = len(nums)
+        output = []
+        backtrack()
+        return output
+
+
+if __name__ == '__main__':
+    nums = [1, 2, 3]
+    print(Solution().permute(nums))
