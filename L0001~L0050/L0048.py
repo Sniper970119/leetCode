526e46131673e032f6efd963380bc7cabb2798ae
Sniper970119 <359366783@qq.com>
2020-02-11 15:18:05 +0800

    0048

--- L0001~L0050/L0048.py
+++ L0001~L0050/L0048.py
@@ -29,12 +29,12 @@ class Solution(object):
         n = len(matrix)
         # 镜像
         for i in range(n):
-            for j in range(i,n):
+            for j in range(i, n):
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
         print(matrix)
         for i in range(n):
             for j in range((n // 2)):
-                matrix[i][j], matrix[i][n - j-1] = matrix[i][n - j-1], matrix[i][j]
+                matrix[i][j], matrix[i][n - j - 1] = matrix[i][n - j - 1], matrix[i][j]
         return matrix
 
 
