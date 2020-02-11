4677ad5ba6ffe3444ed196f1e808b446b2a84b72
Sniper970119 <359366783@qq.com>
2020-02-11 15:17:46 +0800

    0048

---
+++ L0001~L0050/L0048.py
@@ -0,0 +1,48 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+
+
+class Solution(object):
+    def rotate(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        # 镜像
+        for i in range(n):
+            for j in range(i,n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        print(matrix)
+        for i in range(n):
+            for j in range((n // 2)):
+                matrix[i][j], matrix[i][n - j-1] = matrix[i][n - j-1], matrix[i][j]
+        return matrix
+
+
+if __name__ == '__main__':
+    matrix = [
+        [5, 1, 9, 11],
+        [2, 4, 8, 10],
+        [13, 3, 6, 7],
+        [15, 14, 12, 16]
+    ]
+    print(Solution().rotate(matrix))
