65762db2538a1e111744a1531f4e922f40f7d580
Sniper970119 <359366783@qq.com>
2020-02-17 10:49:02 +0800

    L0053

---
+++ L0051~L0100/L0053.py
@@ -0,0 +1,44 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+
+
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) <= 0:
+            return
+        if len(nums) == 1:
+            return nums[0]
+        dp = [0] * len(nums)
+        for i in range(len(nums)):
+            dp[i] = nums[i]
+        for i in range(1, len(nums)):
+            if dp[i] < nums[i] + dp[i - 1]:
+                dp[i] = nums[i] + dp[i - 1]
+        return max(dp)
+
+
+if __name__ == '__main__':
+    nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+    print(Solution().maxSubArray(nums))
