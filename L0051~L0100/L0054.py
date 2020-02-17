26634e89d92bcf70f03472c55d78142bf8d67fda
Sniper970119 <359366783@qq.com>
2020-02-17 12:34:40 +0800

    L0054

---
+++ L0051~L0100/L0054.py
@@ -0,0 +1,54 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+
+
+class Solution(object):
+    def spiralOrder(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[int]
+        """
+        ret = []
+        if matrix == []:
+            return ret
+        ret.extend(matrix[0])  # 上侧
+        # 生成一个反向迭代器，比如1，2，3生成3 2 1
+        new = [reversed(i) for i in matrix[1:]]
+        if new == []:
+            return ret
+        # zip用来解压迭代器 相当于把new生成的迭代器依次遍历，达到一个类似“逆时针旋转”的样子。
+        r = self.spiralOrder([i for i in zip(*new)])
+        ret.extend(r)
+        return ret
+
+
+if __name__ == '__main__':
+    matrix = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
+    matrix = [
+        [1, 2, 3, 4],
+        [5, 6, 7, 8],
+        [9, 10, 11, 12]
+    ]
+    print(Solution().spiralOrder(matrix))
