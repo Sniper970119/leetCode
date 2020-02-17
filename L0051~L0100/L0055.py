360396f1cc9e30a72247a249cc005b7ce8335ff2
Sniper970119 <359366783@qq.com>
2020-02-17 13:33:50 +0800

    L0055

---
+++ L0051~L0100/L0055.py
@@ -0,0 +1,40 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+
+
+class Solution(object):
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        start = 0
+        end = 0
+        n = len(nums)
+        while start <= end and end < len(nums) - 1:
+            end = max(end, nums[start] + start)
+            start += 1
+        return end >= n - 1
+
+
+if __name__ == '__main__':
+    nums = [3, 2, 1, 0, 4]
+    print(Solution().canJump(nums))
