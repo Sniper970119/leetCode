f56fa02666d5b36281e7d6b12c05fbf3a7ce1c4e
Sniper970119 <359366783@qq.com>
2020-02-19 11:04:56 +0800

    L0058

---
+++ L0051~L0100/L0058.py
@@ -0,0 +1,36 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+
+
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        s = s.strip(' ')
+        L = s.split(' ')[-1]
+        return len(L)
+
+
+if __name__ == '__main__':
+    s = "Hello World"
+    print(Solution().lengthOfLastWord(s))
