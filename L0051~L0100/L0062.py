36c854fafb9b61eaf4ec6e8b23619cd18e2c459d
Sniper970119 <359366783@qq.com>
2020-02-21 11:50:27 +0800

    L0062

---
+++ L0051~L0100/L0062.py
@@ -0,0 +1,46 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+
+
+class Solution(object):
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        if m == 1 or n == 1:
+            return 1
+        m = m - 1
+        n = n - 1
+        res = 1
+        for i in range(m + n, m, -1):
+            res *= i
+        for i in range(1, n + 1):
+            res /= i
+
+        return int(res)
+
+
+if __name__ == '__main__':
+    m = 6
+    n = 3
+    print(Solution().uniquePaths(m, n))
