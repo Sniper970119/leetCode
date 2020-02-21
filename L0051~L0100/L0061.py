b535b35875e8cb14b6cdc8d7c609c0cfd4e25af8
Sniper970119 <359366783@qq.com>
2020-02-21 11:10:16 +0800

    L0061

---
+++ L0051~L0100/L0061.py
@@ -0,0 +1,49 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution(object):
+    def rotateRight(self, head, k):
+        """
+        :type head: ListNode
+        :type k: int
+        :rtype: ListNode
+        """
+        if not head: return None
+        orig_head, cnt = head, 1
+        while head.next:
+            head, cnt = head.next, cnt + 1
+        head.next = orig_head
+
+        step = cnt - k % cnt - 1
+        while step > 0:
+            orig_head, step = orig_head.next, step - 1
+
+        new_head, orig_head.next = orig_head.next, None
+        return new_head
+        pass
