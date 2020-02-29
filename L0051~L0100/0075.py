e5e0346ce04a9659743b05096deaa99b9cc31548
Sniper970119 <359366783@qq.com>
2020-02-29 12:01:28 +0800

    L0075

---
+++ L0051~L0100/0075.py
@@ -0,0 +1,39 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+
+
+class Solution(object):
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        for i in range(n):
+            for j in range(i, n):
+                if nums[i] > nums[j]:
+                    nums[i], nums[j] = nums[j], nums[i]
+        return nums
+
+
+if __name__ == '__main__':
+    nums = [2, 0, 2, 1, 1, 0]
+    print(Solution().sortColors(nums))
