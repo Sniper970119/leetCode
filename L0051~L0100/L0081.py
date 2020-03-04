a1161f06458d6a103878204362b2ea92e633a017
Sniper970119 <359366783@qq.com>
2020-03-04 13:02:17 +0800

    L0081

---
+++ L0051~L0100/L0081.py
@@ -0,0 +1,39 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+
+
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: bool
+        """
+        for each in nums:
+            if each == target:
+                return True
+        return False
+
+
+if __name__ == '__main__':
+    nums = [2, 5, 6, 0, 0, 1, 2]
+    target = 0
+    print(Solution().search(nums, target))
