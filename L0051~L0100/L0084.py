b2a99c096974325216a8d2df4987e26985bac978
Sniper970119 <359366783@qq.com>
2020-03-05 11:28:36 +0800

    L0084

---
+++ L0051~L0100/L0084.py
@@ -0,0 +1,41 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+import math
+
+
+class Solution(object):
+    def largestRectangleArea(self, heights):
+        """
+        :type heights: List[int]
+        :rtype: int
+        """
+        n, heights, st, ans = len(heights), [0] + heights + [0], [], 0
+        for i in range(n + 2):
+            while st and heights[st[-1]] > heights[i]:
+                ans = max(ans, heights[st.pop(-1)] * (i - st[-1] - 1))
+            st.append(i)
+        return ans
+
+
+if __name__ == '__main__':
+    heights = [2, 1, 5, 6, 2, 3]
+    heights = [9, 0, 9]
+    print(Solution().largestRectangleArea(heights))
