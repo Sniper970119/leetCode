9db9fcbbc9a9beda86bfbbf78056934d1ec02302
Sniper970119 <359366783@qq.com>
2020-03-06 10:40:21 +0800

    L0085

---
+++ L0051~L0100/L0085.py
@@ -0,0 +1,78 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+
+
+class Solution(object):
+    def maximalRectangle(self, matrix):
+        """
+        :type matrix: List[List[str]]
+        :rtype: int
+        """
+        if not matrix: return 0
+
+        m = len(matrix)
+        n = len(matrix[0])
+
+        left = [0] * n  # initialize left as the leftmost boundary possible
+        right = [n] * n  # initialize right as the rightmost boundary possible
+        height = [0] * n
+
+        maxarea = 0
+
+        for i in range(m):
+
+            cur_left, cur_right = 0, n
+            # update height
+            for j in range(n):
+                if matrix[i][j] == '1':
+                    height[j] += 1
+                else:
+                    height[j] = 0
+            # update left
+            for j in range(n):
+                if matrix[i][j] == '1':
+                    left[j] = max(left[j], cur_left)
+                else:
+                    left[j] = 0
+                    cur_left = j + 1
+            # update right
+            for j in range(n - 1, -1, -1):
+                if matrix[i][j] == '1':
+                    right[j] = min(right[j], cur_right)
+                else:
+                    right[j] = n
+                    cur_right = j
+            # update the area
+            for j in range(n):
+                maxarea = max(maxarea, height[j] * (right[j] - left[j]))
+
+        return maxarea
+
+
+
+if __name__ == '__main__':
+    matrix = [
+        ["1", "0", "0", "0", "0"],
+        ["1", "0", "1", "1", "1"],
+        ["1", "1", "1", "1", "1"],
+        ["1", "0", "0", "1", "0"]
+    ]
+    print(Solution().maximalRectangle(matrix))
