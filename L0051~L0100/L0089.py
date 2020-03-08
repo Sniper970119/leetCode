ca873e4f75bc89ab6d0617035ed1e7cae5e350b8
Sniper970119 <359366783@qq.com>
2020-03-08 11:56:36 +0800

    L0089

---
+++ L0051~L0100/L0089.py
@@ -0,0 +1,37 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+
+
+class Solution(object):
+    def grayCode(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+        res = []
+        for i in range(1 << n):
+            res.append(i ^ i >> 1)
+        return res
+
+
+if __name__ == '__main__':
+    n = 3
+    print(Solution().grayCode(n))
