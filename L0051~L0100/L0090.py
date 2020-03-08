49eeccee05c387aeb866d9224856a78b4cbf5170
Sniper970119 <359366783@qq.com>
2020-03-08 12:13:31 +0800

    L0090

---
+++ L0051~L0100/L0090.py
@@ -0,0 +1,43 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+
+
+class Solution(object):
+    def subsetsWithDup(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        dic = {}
+        for i in nums:
+            dic[i] = dic.get(i, 0) + 1
+        res = [[]]
+        for i, v in dic.items():
+            temp = res.copy()
+            for j in res:
+                temp.extend(j + [i] * (k + 1) for k in range(v))
+            res = temp
+        return res
+
+
+if __name__ == '__main__':
+    nums = [1, 2, 2]
+    print(Solution().subsetsWithDup(nums))
