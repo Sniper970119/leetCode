4a1cd0b3f5898ba91806c7cca4f70a8b09f79eb7
Sniper970119 <359366783@qq.com>
2020-03-09 12:31:56 +0800

    L0092

---
+++ L0051~L0100/L0092.py
@@ -0,0 +1,53 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+
+
+class Solution:
+    def reverseBetween(self, head, m, n):
+        """
+        :type head: ListNode
+        :type m: int
+        :type n: int
+        :rtype: ListNode
+        """
+
+        if not head:
+            return None
+
+        left, right = head, head
+        stop = False
+
+        def recurseAndReverse(right, m, n):
+            nonlocal left, stop
+            if n == 1:
+                return
+            right = right.next
+            if m > 1:
+                left = left.next
+            recurseAndReverse(right, m - 1, n - 1)
+            if left == right or right.next == left:
+                stop = True
+            if not stop:
+                left.val, right.val = right.val, left.val
+                left = left.next
+
+        recurseAndReverse(right, m, n)
+        return head
