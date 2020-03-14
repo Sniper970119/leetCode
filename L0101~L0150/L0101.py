817786e6a47104a1fafc39676a6d32901968e0ba
Sniper970119 <359366783@qq.com>
2020-03-14 12:48:01 +0800

    L0101

---
+++ L0101~L0150/L0101.py
@@ -0,0 +1,46 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+
+        def check(node1, node2):
+            if not node1 and not node2:
+                return True
+            elif not node1 or not node2:
+                return False
+
+            if node1.val != node2.val:
+                return False
+            return check(node1.left, node2.right) and check(node1.right, node2.left)
+
+        return check(root, root)
