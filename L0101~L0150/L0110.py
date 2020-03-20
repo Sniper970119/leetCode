b45422aab17d1fd602e0ee3c3923a2fe958d43d2
Sniper970119 <359366783@qq.com>
2020-03-20 08:42:35 +0800

    L0110

---
+++ L0101~L0150/L0110.py
@@ -0,0 +1,55 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def isBalanced(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+
+        def ib(node):
+            if node is None:
+                return 0
+            l_depth = ib(node.left)
+            r_depth = ib(node.right)
+            assert abs(l_depth - r_depth) <= 1
+            return max(l_depth, r_depth) + 1
+
+        try:
+            ib(root)
+        except:
+            return False
+        else:
+            return True
+
+
+if __name__ == '__main__':
+    pass
