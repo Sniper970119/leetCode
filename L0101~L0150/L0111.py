ca6498f852746f0ee05107bd5bebfdec01863641
Sniper970119 <359366783@qq.com>
2020-03-21 10:18:58 +0800

    L0111

---
+++ L0101~L0150/L0111.py
@@ -0,0 +1,65 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def minDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+
+        def dfs(root, level):
+            if root is None:
+                return level
+            return min(dfs(root.left, level + 1), dfs(root.right, level + 1))
+
+        if root is None:
+            return 0
+        a = dfs(root, 0)
+        return dfs(root, 0)
+
+
+if __name__ == '__main__':
+    # root = TreeNode(1)
+    # root.left = TreeNode(2)
+    root = TreeNode(3)
+    n1 = TreeNode(9)
+    n2 = TreeNode(20)
+    n3 = TreeNode(None)
+    n4 = TreeNode(None)
+    n5 = TreeNode(15)
+    n6 = TreeNode(7)
+    root.left = n1
+    root.right = n2
+    n1.left = n3
+    n1.right = n3
+    n2.left = n5
+    n2.right = n6
+    print(Solution().minDepth(root))
+    pass
