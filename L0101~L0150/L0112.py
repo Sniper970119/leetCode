c6d96ff3132e793363b81ea6e8374de46fffac25
Sniper970119 <359366783@qq.com>
2020-03-21 10:04:37 +0800

    L0112

---
+++ L0101~L0150/L0112.py
@@ -0,0 +1,51 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def hasPathSum(self, root, sum):
+        """
+        :type root: TreeNode
+        :type sum: int
+        :rtype: bool
+        """
+        if not root:
+            return False
+        if root.val == sum:
+            if not root.left and not root.right:
+                return True
+        sum -= root.val
+        l = self.hasPathSum(root.left, sum)
+        r = self.hasPathSum(root.right, sum)
+        return l or r
+        pass
+
+
+if __name__ == '__main__':
+    # print(Solution().hasPathSum())
+    pass
