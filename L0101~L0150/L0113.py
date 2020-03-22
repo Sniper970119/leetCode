c7c411f801b3ea73dd058aae2127b7d9a7a74bf7
Sniper970119 <359366783@qq.com>
2020-03-22 15:16:35 +0800

    L0013

---
+++ L0101~L0150/L0113.py
@@ -0,0 +1,52 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def pathSum(self, root, sums):
+        """
+        :type root: TreeNode
+        :type sum: int
+        :rtype: List[List[int]]
+        """
+        a = []
+        if not root:
+            return []
+
+        def dfs(root, al):
+            if root.right:
+                dfs(root.right, al + [root.right.val])
+            if root.left:
+                dfs(root.left, al + [root.left.val])
+            if not root.right and not root.left:
+                if sum(al) == sums:
+                    a.append(al)
+
+        dfs(root, [root.val])
+        return a
