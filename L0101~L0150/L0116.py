84af25821c4ab0480392a2a30d2988598f24b8dd
Sniper970119 <359366783@qq.com>
2020-03-25 10:16:46 +0800

    L0116

---
+++ L0101~L0150/L0116.py
@@ -0,0 +1,51 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+
+
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val=0, left=None, right=None, next=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+
+
+class Solution(object):
+    def connect(self, root):
+        """
+        :type root: Node
+        :rtype: Node
+        """
+        if root is None:
+            return
+
+        the_root = root
+
+        while root.left is not None:
+            next_layer = root.left
+            while root.next is not None:
+                root.left.next = root.right
+                root.right.next = root.next.left
+                root = root.next
+            root.left.next = root.right
+            root = next_layer
+        return the_root
