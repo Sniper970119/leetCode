59587fb355de9c97bf6151555f44971cc41512a7
Sniper970119 <359366783@qq.com>
2020-03-25 10:20:03 +0800

    L0117

---
+++ L0101~L0150/L0117.py
@@ -0,0 +1,51 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+
+
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val=0, left=None, right=None, next=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+
+
+class Solution(object):
+    def connect(self, root):
+        """
+        :type root: Node
+        :rtype: Node
+        """
+        left_most = root  # left_most表示当前层的最左边节点
+        while (left_most):  # 每次循环连接当前层的下一层
+            head = tail = Node(0)  # head表示下一层的虚拟头部
+            curr = left_most
+            while (curr):  # 遍历当前层，将下一层连接
+                if (curr.left):
+                    tail.next = curr.left
+                    tail = tail.next
+                if (curr.right):
+                    tail.next = curr.right
+                    tail = tail.next
+                curr = curr.next
+            left_most = head.next
+        return root
