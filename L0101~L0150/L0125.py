e14da549c282b438bc22f8b0713bbfb7503091f8
Sniper970119 <359366783@qq.com>
2020-03-30 10:02:42 +0800

    L0125.py

---
+++ L0101~L0150/L0125.py
@@ -0,0 +1,35 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+
+
+class Solution:
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        s = ''.join(filter(str.isalnum, s)).lower()
+        return s == s[::-1]
+
+
+if __name__ == '__main__':
+    s = "A man, a plan, a canal: Panama"
+    print(Solution().isPalindrome(s))
