89e418caea340bfa844f49173a2dbb83cb58d674
Sniper970119 <359366783@qq.com>
2020-04-11 10:36:53 +0800

    L0137.py

---
+++ L0101~L0150/L0137.py
@@ -0,0 +1,35 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+
+
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        return int((sum(set(nums)) * 3 - sum(nums)) / 2)
+
+
+if __name__ == '__main__':
+    nums = [2, 2, 1, 2]
+    nums = [0, 1, 0, 1, 0, 1, 99]
+    print(Solution().singleNumber(nums))
