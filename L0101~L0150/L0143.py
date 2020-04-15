3ff7a83bf5ea3cf6adc4744b2bf23bc694f2848e
Sniper970119 <359366783@qq.com>
2020-04-15 11:57:16 +0800

    L0143.py

---
+++ L0101~L0150/L0143.py
@@ -0,0 +1,56 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution(object):
+    def reorderList(self, head):
+        """
+        :type head: ListNode
+        :rtype: None Do not return anything, modify head in-place instead.
+        """
+        if not head or not head.next:
+            return head
+        fast, slow = head, head
+        # 找到中点
+        while fast.next and fast.next.next:
+            fast = fast.next.next
+            slow = slow.next
+        # 反转后半链表
+        p, right = slow.next, None
+        slow.next = None
+        while p:
+            right, right.next, p = p, right, p.next
+        # 重排练表
+        left = head
+        while left and right:
+            left.next, right.next, left, right = right, left.next, left.next, right.next
+        pass
+
+
+if __name__ == '__main__':
+    pass
