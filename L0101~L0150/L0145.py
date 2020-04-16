80b6e21b9f00c0c5ebdc135fc2f53ebf0c3275d2
Sniper970119 <359366783@qq.com>
2020-04-16 09:40:36 +0800

    L0145.py

---
+++ L0101~L0150/L0145.py
@@ -0,0 +1,46 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def postorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        ans, stack, cur = [], [], root
+        while len(stack) or cur:
+            if cur:
+                stack.append(cur)
+                ans.append(cur.val)
+                cur = cur.right
+            else:
+                cur = stack.pop()
+                cur = cur.left
+        return ans[::-1]
