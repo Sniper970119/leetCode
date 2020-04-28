96bf4eaf1e0f3b6083c9d8d176e4b360d984bae4
Sniper970119 <359366783@qq.com>
2020-04-28 22:18:59 +0800

    L0165.py

---
+++ L0151~L0200/L0165.py
@@ -0,0 +1,42 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+
+
+class Solution(object):
+    def compareVersion(self, version1, version2):
+        """
+        :type version1: str
+        :type version2: str
+        :rtype: int
+        """
+        v1 = [int(x) for x in version1.split('.')]
+        v2 = [int(x) for x in version2.split('.')]
+        while v1 and v2:
+            x, y = v1.pop(0), v2.pop(0)
+            if x < y:
+                return -1
+            if x > y:
+                return 1
+        if v1 and any(x > 0 for x in v1):
+            return 1
+        if v2 and any(x > 0 for x in v2):
+            return -1
+        return 0
