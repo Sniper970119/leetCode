2b78e37e0d6ef6f336ba44fb53b37f5293cf1034
Sniper970119 <359366783@qq.com>
2020-04-30 16:50:09 +0800

    L0172.py

---
+++ L0151~L0200/L0172.py
@@ -0,0 +1,38 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+
+
+class Solution(object):
+    def trailingZeroes(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        count = 0
+        while n >= 5:
+            count += n / 5
+            n /= 5
+        return int(count)
+
+
+if __name__ == '__main__':
+    n = 50
+    print(Solution().trailingZeroes(n))
