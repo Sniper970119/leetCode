8b29db2d59ca8d1e49f123be4265916aee9bca92
Sniper970119 <359366783@qq.com>
2020-05-02 23:29:58 +0800

    L0175.py

---
+++ L0151~L0200/L0175.py
@@ -0,0 +1,29 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+
+"""
+select p.FirstName,p.LastName,a.City,a.State
+from
+person p left join address a
+on
+p.personid=a.personid
+
+"""
