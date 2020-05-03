77d524d19e6c3ac9afeb80f11ba8af5aa5d5c0af
Sniper970119 <359366783@qq.com>
2020-05-03 11:28:11 +0800

    L0174.py

---
+++ L0151~L0200/L0174.py
@@ -0,0 +1,47 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+
+
+class Solution(object):
+    def calculateMinimumHP(self, dungeon):
+        """
+        :type dungeon: List[List[int]]
+        :rtype: int
+        """
+        n = len(dungeon)
+        m = len(dungeon[0])
+        dp = [[float('inf') for _ in range(m + 1)] for _ in range(n + 1)]
+        dp[n][m - 1] = dp[n - 1][m] = 1
+        for i in range(n - 1, -1, -1):
+            for j in range(m - 1, -1, -1):
+                dp[i][j] = max(1, min(dp[i + 1][j], dp[i][j + 1]) - dungeon[i][j])
+        return dp[0][0]
+        pass
+
+
+if __name__ == '__main__':
+    map = [
+        [-2, -3, 3],
+        [-5, -10, 1],
+        [10, 30, -5]
+    ]
+    print(Solution().calculateMinimumHP(map))
+    pass
