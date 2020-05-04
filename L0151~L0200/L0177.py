8dcd8e065e6da710f18dea09721ab0e7572fc530
Sniper970119 <359366783@qq.com>
2020-05-04 11:18:10 +0800

    L0177.py

---
+++ L0151~L0200/L0177.py
@@ -0,0 +1,31 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+
+"""
+
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+	SET N = N - 1;
+	RETURN (
+	 SELECT DISTINCT salary FROM employee ORDER BY salary DESC LIMIT N, 1
+	);
+END
+"""
