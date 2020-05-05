844ebd9270c582411647773e58afa804c7de115b
Sniper970119 <359366783@qq.com>
2020-05-05 22:02:14 +0800

    L0178.py

---
+++ L0151~L0200/L0178.py
@@ -0,0 +1,32 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+
+'''
+
+select t.score Score,t.rank Rank from scores join
+(
+    SELECT p.score score , @rank:=@rank+1 rank FROM 
+        (SELECT distinct(score) score FROM Scores ORDER BY score DESC) p , (SELECT @rank:=0 ) q
+) t 
+on scores.score = t.score
+order by score desc
+
+'''
