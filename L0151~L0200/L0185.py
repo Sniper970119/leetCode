a57869622ce9d35a5131e383a754afcf4ff46327
Sniper970119 <359366783@qq.com>
2020-05-09 16:31:53 +0800

    L0185.py

---
+++ L0151~L0200/L0185.py
@@ -0,0 +1,33 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+"""
+select d.Name Department,a.Name Employee,a.Salary Salary
+from
+(
+select Name,DepartmentId,Salary,
+dense_rank() over(partition by DepartmentId order by Salary desc) as rnk
+from Employee
+) a 
+inner join 
+Department d 
+on a.DepartmentId=d.Id
+where a.rnk<=3;
+"""
