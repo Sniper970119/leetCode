26f349f7862f014c4fbd330e111b67bcd4e96b9f
Sniper970119 <359366783@qq.com>
2020-05-14 14:02:44 +0800

    L0189.py

---
+++ L0151~L0200/L0189.py
@@ -0,0 +1,42 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+
+
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums) - k
+        temp = nums[n:] + nums[:n]
+        return temp
+        # nums[:] = nums[-k % len(nums):] + nums[: -k % len(nums)]
+        pass
+
+
+if __name__ == '__main__':
+    nums = [1, 2, 3, 4, 5, 6, 7]
+    k = 3
+    # nums = [-1, -100, 3, 99]
+    # k = 2
+    print(Solution().rotate(nums, k))
