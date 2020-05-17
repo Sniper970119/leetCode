255a2bd41837088058d7f789e7a01a8161470b75
Sniper970119 <359366783@qq.com>
2020-05-17 12:16:01 +0800

    L0199.py

---
+++ L0151~L0200/L0199.py
@@ -0,0 +1,38 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def rightSideView(self, root: TreeNode):
+        d = {}
+        def f(r, i):
+            if r:
+                d[i] = r.val
+                f(r.left, i + 1)
+                f(r.right, i + 1)
+        f(root, 0)
+        return [*d.values()]
