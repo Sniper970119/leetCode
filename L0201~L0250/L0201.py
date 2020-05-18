1a3ce57c5c6156e34e8508e0512a756b9c4e3ba0
Sniper970119 <359366783@qq.com>
2020-05-18 11:16:20 +0800

    L0201.py

---
+++ L0201~L0250/L0201.py
@@ -0,0 +1,43 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+
+
+class Solution(object):
+    def rangeBitwiseAnd(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        i = 0
+        while m != n:
+            m >>= 1
+            n >>= 1
+            i += 1
+
+        return m << i
+
+
+
+if __name__ == '__main__':
+    m = 5
+    n = 7
+    print(Solution().rangeBitwiseAnd(m, n))
