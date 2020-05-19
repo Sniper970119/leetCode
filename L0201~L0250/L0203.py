a4a753f682819e5bff0b1b1bd6b56582a7eade21
Sniper970119 <359366783@qq.com>
2020-05-19 12:17:09 +0800

    L0203.py

---
+++ L0201~L0250/L0203.py
@@ -0,0 +1,45 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def removeElements(self, head, val):
+        """
+        :type head: ListNode
+        :type val: int
+        :rtype: ListNode
+        """
+        node = ListNode(val + 1)
+        node.next = head
+        pre, cur = node, head
+        while cur:
+            if cur.val == val:
+                pre.next = cur.next
+                cur = cur.next
+            else:
+                pre = cur
+                cur = cur.next
+        return node.next
