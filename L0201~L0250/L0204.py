d3b43438a8a9711d30a65b88090f956eff412352
Sniper970119 <359366783@qq.com>
2020-05-19 12:26:31 +0800

    L0204.py

---
+++ L0201~L0250/L0204.py
@@ -0,0 +1,42 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+
+
+class Solution(object):
+    def countPrimes(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n < 3:
+            return 0
+        else:
+            result = [1] * n
+            result[0], result[1] = 0, 0
+            for i in range(2, int(n ** 0.5) + 1):
+                if result[i] == 1:
+                    result[i * i:n:i] = [0] * len(result[i * i:n:i])
+        return sum(result)
+
+
+if __name__ == '__main__':
+    n = 10
+    print(Solution().countPrimes(n))
