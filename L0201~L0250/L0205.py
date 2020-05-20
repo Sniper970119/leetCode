51102cf6c6f589bf17a99ea430dcd6bc2fcac3dd
Sniper970119 <359366783@qq.com>
2020-05-20 11:05:31 +0800

    L0205.py

---
+++ L0201~L0250/L0205.py
@@ -0,0 +1,39 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+
+
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        for i in range(len(s)):
+            if s[i + 1:].find(s[i]) != t[i + 1:].find(t[i]):
+                return False
+        return True
+
+
+if __name__ == '__main__':
+    s = 'foo'
+    t = 'bar'
+    print(Solution().isIsomorphic(s, t))
