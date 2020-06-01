e94dc32b70f416889a3c17bdff713e28f0ea2cf6
Sniper970119 <359366783@qq.com>
2020-06-01 12:05:40 +0800

    L0217.py

---
+++ L0201~L0250/L0217.py
@@ -0,0 +1,35 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+
+
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        return len(nums) != len(set(nums))
+
+
+if __name__ == '__main__':
+    nums = [1, 2, 3, 1]
+    nums = [1, 2, 3, 4]
+    print(Solution().containsDuplicate(nums))
