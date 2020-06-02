9a35332a4aab549e77e76c89ffa71e0ed3e0e4d3
Sniper970119 <359366783@qq.com>
2020-06-02 11:40:25 +0800

    L0216.py

---
+++ L0201~L0250/L0216.py
@@ -0,0 +1,49 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+
+
+class Solution(object):
+    def combinationSum3(self, k, n):
+        """
+        :type k: int
+        :type n: int
+        :rtype: List[List[int]]
+        """
+        res_list = []
+
+        def back(idx, temp_list):
+            if len(temp_list) == k:
+                if sum(temp_list) == n:
+                    res_list.append(temp_list)
+                return
+            for i in range(idx + 1, 10):
+                temp_list.append(i)
+                back(i, temp_list[:])
+                temp_list = temp_list[:-1]
+
+        back(0, [])
+        return res_list
+
+
+if __name__ == '__main__':
+    k = 3
+    n = 15
+    print(Solution().combinationSum3(k, n))
