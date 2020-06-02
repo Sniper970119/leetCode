592996bddcdd5fe4580b3c8d515d236272b47b3c
Sniper970119 <359366783@qq.com>
2020-06-02 11:17:43 +0800

    L0215.py

---
+++ L0201~L0250/L0215.py
@@ -0,0 +1,39 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+
+
+class Solution(object):
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        nums.sort()
+        print(nums)
+        return nums[-k]
+
+
+if __name__ == '__main__':
+    nums = [3, 2, 3, 1, 2, 4, 5, 5, 6]
+    nums = [3, 2, 1, 5, 6, 4]
+    k = 2
+    print(Solution().findKthLargest(nums, k))
