2dfbce5500ffcbf41d377d153eda9b86baed8416
Sniper970119 <359366783@qq.com>
2020-06-03 12:21:38 +0800

    L0219.py

---
+++ L0201~L0250/L0219.py
@@ -0,0 +1,41 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+
+
+class Solution(object):
+    def containsNearbyDuplicate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
+        for i in range(len(nums)):
+            for j in range(i + 1, k + i + 1):
+                if j < len(nums) and nums[i] == nums[j]:
+                    return True
+        return False
+
+
+if __name__ == '__main__':
+    nums = [1, 2, 3, 1]
+    nums = [2,2]
+    k = 3
+    print(Solution().containsNearbyDuplicate(nums, k))
