a816f994eb6c4d622a75af428e15f5487832c661
Sniper970119 <359366783@qq.com>
2020-06-04 12:56:42 +0800

    L0221.py

---
+++ L0201~L0250/L0221.py
@@ -0,0 +1,51 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+
+
+class Solution(object):
+    def maximalSquare(self, matrix):
+        """
+        :type matrix: List[List[str]]
+        :rtype: int
+        """
+        if len(matrix) == 0 or len(matrix[0]) == 0:
+            return 0
+
+        maxSide = 0
+        rows, columns = len(matrix), len(matrix[0])
+        dp = [[0] * columns for _ in range(rows)]
+        for i in range(rows):
+            for j in range(columns):
+                if matrix[i][j] == '1':
+                    if i == 0 or j == 0:
+                        dp[i][j] = 1
+                    else:
+                        dp[i][j] = min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1
+                    maxSide = max(maxSide, dp[i][j])
+
+        maxSquare = maxSide * maxSide
+        return maxSquare
+
+
+if __name__ == '__main__':
+    matrix = [["1", "0", "1", "0", "0"], ["1", "0", "1", "1", "1"], ["1", "1", "1", "1", "1"],
+              ["1", "0", "0", "1", "0"]]
+    print(Solution().maximalSquare(matrix))
