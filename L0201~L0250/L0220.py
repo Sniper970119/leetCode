0045d383cc70a2a565c622ac3798a08658fc12a1
Sniper970119 <359366783@qq.com>
2020-06-04 12:39:42 +0800

    L0220.py

---
+++ L0201~L0250/L0220.py
@@ -0,0 +1,44 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+
+
+class Solution(object):
+    def containsNearbyAlmostDuplicate(self, nums, k, t):
+        """
+        :type nums: List[int]
+        :type k: int
+        :type t: int
+        :rtype: bool
+        """
+        for i in range(len(nums)):
+            for j in range(i + 1, k + i + 1):
+                if j < len(nums) and abs(nums[i] - nums[j]) <= t:
+                    return True
+        return False
+        pass
+
+
+if __name__ == '__main__':
+    nums = [1, 2, 3, 1]
+    nums = [1, 5, 9, 1, 5, 9]
+    k = 2
+    t = 3
+    print(Solution().containsNearbyAlmostDuplicate(nums, k, t))
