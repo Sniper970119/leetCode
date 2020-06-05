bafa87794a28a9cf8fcc1c369b7da2d4defc3f32
Sniper970119 <359366783@qq.com>
2020-06-05 11:54:50 +0800

    L0223.py

---
+++ L0201~L0250/L0223.py
@@ -0,0 +1,42 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+
+
+class Solution(object):
+    def computeArea(self, A, B, C, D, E, F, G, H):
+        """
+        :type A: int
+        :type B: int
+        :type C: int
+        :type D: int
+        :type E: int
+        :type F: int
+        :type G: int
+        :type H: int
+        :rtype: int
+        """
+        # 如果不重叠，就是两个面积相加的和
+        if H <= B or E >= C or G <= A or F >= D:
+            return (G - E) * (H - F) + (C - A) * (D - B)
+        # 包含关系
+        col = sorted([A, E, C, G])
+        row = sorted([B, F, D, H])
+        return (G - E) * (H - F) + (C - A) * (D - B) - (col[2] - col[1]) * (row[2] - row[1])
