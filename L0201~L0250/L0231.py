144a8afb4abfd8b645e16861d48c9d6a535a6c0d
Sniper970119 <359366783@qq.com>
2020-06-16 10:40:56 +0800

    L0231.py

---
+++ L0201~L0250/L0231.py
@@ -0,0 +1,44 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+import math
+
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n == 1:
+            return True
+        if n == 0:
+            return False
+        while n > 2:
+            # n = math.sqrt(n)
+            n = n / 2
+        if n == 2:
+            return True
+        else:
+            return False
+
+
+if __name__ == '__main__':
+    print(Solution().isPowerOfTwo(5))
+
