162c555d763d70718b94191118922af9da495360
Sniper970119 <359366783@qq.com>
2020-06-27 10:51:34 +0800

    L0233.py

---
+++ L0201~L0250/L0233.py
@@ -0,0 +1,41 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+
+
+class Solution(object):
+    def countDigitOne(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        res = 0
+        i = 1
+        while i <= n:
+            divider = i * 10
+            res += (n // divider) * i + min(max(n % divider - i + 1, 0), i)
+            i *= 10
+        return res
+
+
+
+if __name__ == '__main__':
+    n = 13
+    print(Solution().countDigitOne(n))
