4f9fcaa2bc69941840007e2769e8c310c89288b6
Sniper970119 <359366783@qq.com>
2020-06-30 16:03:29 +0800

    L0257.py

---
+++ L0251~L0300/L0257.py
@@ -0,0 +1,46 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def binaryTreePaths(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[str]
+        """
+        if not root:
+            return []
+        if not root.left and not root.right:
+            return [str(root.val)]
+        paths = []
+        if root.left:
+            for i in self.binaryTreePaths(root.left):
+                paths.append(str(root.val) + '->' + i)
+        if root.right:
+            for i in self.binaryTreePaths(root.right):
+                paths.append(str(root.val) + '->' + i)
+        return paths
