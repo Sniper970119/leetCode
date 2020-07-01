50f4679f1aff0b61e2fe281faac55524a90d54b7
Sniper970119 <359366783@qq.com>
2020-07-01 10:15:29 +0800

    L0240.py

---
+++ L0201~L0250/L0240.py
@@ -0,0 +1,55 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+
+
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        if len(matrix) == 0:
+            return False
+        if len(matrix[0]) == 0:
+            return False
+        for i in range(len(matrix)):
+            if matrix[i][0] > target:
+                break
+            for j in range(len(matrix[0])):
+                if matrix[i][j] == target:
+                    return True
+                if matrix[i][j] > target:
+                    break
+        return False
+        pass
+
+
+if __name__ == '__main__':
+    matrix = [
+        [1, 4, 7, 11, 15],
+        [2, 5, 8, 12, 19],
+        [3, 6, 9, 16, 22],
+        [10, 13, 14, 17, 24],
+        [18, 21, 23, 26, 30]
+    ]
+    target = 20
+    print(Solution().searchMatrix(matrix, target))
