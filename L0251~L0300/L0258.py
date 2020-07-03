b84b21db69a7e7b7350a234cc0d08adee3354fd8
Sniper970119 <359366783@qq.com>
2020-07-03 12:32:51 +0800

    L0258.py

---
+++ L0251~L0300/L0258.py
@@ -0,0 +1,41 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+
+
+class Solution(object):
+    def addDigits(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        res = str(num)
+        while len(res) != 1:
+            temp = 0
+            for i in range(len(res)):
+                temp += int(res[i])
+            res = str(temp)
+        return int(res)
+        pass
+
+
+if __name__ == '__main__':
+    n = 38
+    print(Solution().addDigits(n))
