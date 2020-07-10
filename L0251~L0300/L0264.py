e9e7ba2b99fc6ae79b91c534f57b3a961a99eab6
Sniper970119 <359366783@qq.com>
2020-07-10 14:34:27 +0800

    L0264.py

---
+++ L0251~L0300/L0264.py
@@ -0,0 +1,49 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+
+
+class Ugly:
+    def __init__(self):
+        self.nums = nums = [1, ]
+        i2 = i3 = i5 = 0
+
+        for i in range(1, 1690):
+            ugly = min(nums[i2] * 2, nums[i3] * 3, nums[i5] * 5)
+            nums.append(ugly)
+
+            if ugly == nums[i2] * 2:
+                i2 += 1
+            if ugly == nums[i3] * 3:
+                i3 += 1
+            if ugly == nums[i5] * 5:
+                i5 += 1
+
+
+class Solution:
+    u = Ugly()
+
+    def nthUglyNumber(self, n):
+        return self.u.nums[n - 1]
+
+
+if __name__ == '__main__':
+    n = 10
+    print(Solution().nthUglyNumber(n))
