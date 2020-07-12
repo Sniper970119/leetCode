f0412a386d89c9e9e740cb0b1681dbd1ef982d06
Sniper970119 <359366783@qq.com>
2020-07-12 15:45:39 +0800

    L0268.py

---
+++ L0251~L0300/L0268.py
@@ -0,0 +1,36 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        number1 = 0
+        number2 = 0
+
+        for i in range(len(nums)+1):
+            number1 ^= i
+        for j in nums:
+            number2 ^= j
+
+        return number1^number2
