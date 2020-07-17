2ea698094793ec149ece8e8a96976fc6c3642a0d
Sniper970119 <359366783@qq.com>
2020-07-17 21:41:33 +0800

    L0274.py

---
+++ L0251~L0300/L0274.py
@@ -0,0 +1,38 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+
+
+class Solution(object):
+    def hIndex(self, citations):
+        """
+        :type citations: List[int]
+        :rtype: int
+        """
+        citations = sorted(citations, reverse=True)
+        for i in range(len(citations)):
+            if i + 1 > citations[i]:
+                return i
+        return len(citations)
+
+
+if __name__ == '__main__':
+    citations = [3, 0, 6, 1, 5]
+    print(Solution().hIndex(citations))
