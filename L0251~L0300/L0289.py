95bb949c23c01e9333d2bf43297fc650eb279d18
Sniper970119 <359366783@qq.com>
2020-08-18 18:38:40 +0800

    L0289.py

--- L0251~L0300/L0289.py
+++ L0251~L0300/L0289.py
@@ -25,5 +25,5 @@ class Solution(object):
         :type board: List[List[int]]
         :rtype: None Do not return anything, modify board in-place instead.
         """
-
+        print()
         pass
