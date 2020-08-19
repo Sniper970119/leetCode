e086566728de599dca9ff6c870e213e1cbb7a3c8
Sniper970119 <359366783@qq.com>
2020-08-19 20:14:05 +0800

    L0344.py

---
+++ L0301~L0350/L0344.py
@@ -0,0 +1,39 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+
+
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
+        # s = s[::-1]
+        s.reverse()
+        return s
+
+
+if __name__ == '__main__':
+    s = ["h", "e", "l", "l", "o"]
+    print(id(s))
+    s = Solution().reverseString(s)
+    print(s)
+    print(id(s))
