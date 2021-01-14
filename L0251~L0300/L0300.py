97fbe29bf711371b541b8017b2601d88e11699f1
Sniper <359366783@qq.com>
2021-01-14 12:42:07 +0800

    L0300.py

---
+++ L0251~L0300/L0300.py
@@ -0,0 +1,49 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃CREATE BY SNIPER┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+
+"""
+
+
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) == 0:
+            return 0
+        if len(nums) == 1:
+            return 1
+        flag = 1
+        max_flag = 1
+        for i in range(1, len(nums)):
+            if nums[i] > nums[i - 1]:
+                flag += 1
+            else:
+                max_flag = max(flag, max_flag)
+                flag = 1
+        return max_flag
+        pass
+
+
+if __name__ == '__main__':
+    nums = [10, 9, 2, 5, 3, 7, 101, 18]
+    print(Solution().lengthOfLIS(nums))
+    pass
