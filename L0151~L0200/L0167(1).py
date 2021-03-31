99abfa181a4772df48c47bd71de2d7ee69907685
Sniper <359366783@qq.com>
2021-03-31 21:59:36 +0800

    L0167(1).py

---
+++ L0151~L0200/L0167(1).py
@@ -0,0 +1,45 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃CREATE BY SNIPER┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+
+"""
+
+
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        i, j = 0, len(numbers) - 1
+        while i != j:
+            if numbers[i] + numbers[j] == target:
+                break
+            elif numbers[i] + numbers[j] < target:
+                i += 1
+            elif numbers[i] + numbers[j] > target:
+                j -= 1
+        return [i+1, j+1]
+
+
+if __name__ == '__main__':
+    numbers = [2, 7, 11, 15]
+    target = 9
+    print(Solution().twoSum(numbers, target))
