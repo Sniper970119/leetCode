e02534a66949e1b96cb72661f7e730ae9973f56f
Sniper <359366783@qq.com>
2021-04-02 20:46:46 +0800

    L0633.py

--- L0600~L0650/L0633.py
+++ L0600~L0650/L0633.py
@@ -19,3 +19,29 @@
           ┗━┻━┛   ┗━┻━┛
 
 """
+
+
+class Solution(object):
+    def judgeSquareSum(self, c):
+        """
+        :type c: int
+        :rtype: bool
+        """
+        import math
+        max_idx = int(math.sqrt(c))
+        i, j = 0, max_idx + 1
+        while i <= j:
+            res = i * i + j * j
+            if res > c:
+                j -= 1
+            elif res < c:
+                i += 1
+            else:
+                return True
+        return False
+
+
+if __name__ == '__main__':
+    c = 2
+    print(Solution().judgeSquareSum(c))
+    pass
---
+++ L0600~L0650/README.md
@@ -0,0 +1,3 @@
+# Sniper
+
+[L0633](https://leetcode-cn.com/problems/sum-of-square-numbers/description/): 双指针法
