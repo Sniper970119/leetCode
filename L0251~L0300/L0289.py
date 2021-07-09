8a48b0f74d15a224708a33983bf497d436c50e43
Yu <zhaoyu@sniper97.cn>
2021-07-09 15:47:39 +0800

    L0289

--- L0251~L0300/L0289.py
+++ L0251~L0300/L0289.py
@@ -12,18 +12,41 @@
         ┃　　　┃   神兽保佑
         ┃　　　┃   代码无BUG！
         ┃　　　┗━━━━━━━━━┓
-        ┃　　　　　　　    ┣┓
+        ┃CREATE BY SNIPER┣┓
         ┃　　　　         ┏┛
         ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
           ┃ ┫ ┫   ┃ ┫ ┫
           ┗━┻━┛   ┗━┻━┛
 """
 
-class Solution(object):
+
+class Solution:
     def gameOfLife(self, board):
         """
-        :type board: List[List[int]]
-        :rtype: None Do not return anything, modify board in-place instead.
+        Do not return anything, modify board in-place instead.
         """
-        print()
-        pass
+        neighbors = [(1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1)]
+        rows = len(board)
+        cols = len(board[0])
+        copy_board = [[board[row][col] for col in range(cols)] for row in range(rows)]
+        for row in range(rows):
+            for col in range(cols):
+                live_neighbors = 0
+                for neighbor in neighbors:
+
+                    r = (row + neighbor[0])
+                    c = (col + neighbor[1])
+                    if (r < rows and r >= 0) and (c < cols and c >= 0) and (copy_board[r][c] == 1):
+                        live_neighbors += 1
+
+                if copy_board[row][col] == 1 and (live_neighbors < 2 or live_neighbors > 3):
+                    board[row][col] = 0
+                if copy_board[row][col] == 0 and live_neighbors == 3:
+                    board[row][col] = 1
+        return board
+
+
+
+if __name__ == '__main__':
+    board = [[0, 1, 0], [0, 0, 1], [1, 1, 1], [0, 0, 0]]
+    print(Solution().gameOfLife(board))
