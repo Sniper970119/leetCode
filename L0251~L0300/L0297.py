758a621eaefac851e054e8275cca19d9d3b335ef
Yu <zhaoyu@sniper97.cn>
2021-07-12 10:43:39 +0800

    L0297

---
+++ L0251~L0300/L0297.py
@@ -0,0 +1,58 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃CREATE BY SNIPER┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Codec:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+
+        :type root: TreeNode
+        :rtype: str
+        """
+        import pickle
+        s = pickle.dumps(root)
+        return s
+
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+
+        :type data: str
+        :rtype: TreeNode
+        """
+        import pickle
+        return pickle.loads(data)
+
+
+# Your Codec object will be instantiated and called as such:
+ser = Codec()
+ser.serialize(TreeNode(1))
+# deser = Codec()
+# ans = deser.deserialize(ser.serialize(root))
